eec662e84fa13dc4552e888dfce2f434b1f8c398
KnowsCount <knowscount@gmail.com>
2021-06-11 21:38:48 +0800

    feat: init 🎸

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import Qiokian from '@/qiokian.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Qiokian
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <qiokian />
+  </div>
+</template>
