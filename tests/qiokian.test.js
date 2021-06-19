f3c2782fe6a0a1f495e8e5bbbdafdd1f35e5cf3e
KnowsCount <knowscount@gmail.com>
2021-06-20 00:55:01 +0800

    chore: enhance qiokian.test.js

--- tests/qiokian.test.js
+++ tests/qiokian.test.js
@@ -1,20 +1,17 @@
 /*
  * @Date: 11/07/2016 16.00.01 +0800
  * @Author: KnowsCount
- * @LastEditTime: 19/06/2021 20.16.53 +0800
+ * @LastEditTime: 20/06/2021 00.54.39 +0800
  * @FilePath: /qiokian/tests/qiokian.test.js
  */
 
 /* eslint-disable no-undef */
 
 import qiokian from '../src/qiokian.vue'
+import { shallowMount } from '@vue/test-utils'
 import assert from 'assert'
 
 describe('qiokian', function() {
-	// it('should export a function', function() {
-	// 	assert.equal(typeof qiokian, 'function')
-	// })
-
 	it('should export an object', function() {
 		assert(qiokian)
 		assert.strictEqual(typeof qiokian, 'object')
@@ -25,4 +22,17 @@ describe('qiokian', function() {
 			qiokian()
 		})
 	})
+
+	const wrapper = shallowMount(qiokian)
+	it('should contain a <button> & <div> element', function() {
+		// expect(wrapper.find('button')).toExist()
+		// The expect assertion was handed over to the Jest team, who in their infinite wisdom, created a new API.
+		expect(wrapper.find('button')).toBeTruthy()
+		expect(wrapper.find('div')).toBeTruthy()
+	})
+
+	// if (screen.width < 768) {
+	// 	expect(wrapper.find('button')).toBeFalsy()
+	// }
+	// what happened to this part?... it says `Received: {"selector": "button"}`
 })
