6690fb2f4bd08d7e57cba9b293ef3ea0b8d4ddd0
KnowsCount <knowscount@gmail.com>
2021-06-19 20:16:43 +0800

    refactor: switching from `mocha` to `jest`

--- tests/qiokian.test.js
+++ tests/qiokian.test.js
@@ -1,17 +1,14 @@
 /*
  * @Date: 11/07/2016 16.00.01 +0800
  * @Author: KnowsCount
- * @LastEditTime: 18/06/2021 07.26.32 +0800
- * @FilePath: /qiokian/tests/test.js
+ * @LastEditTime: 19/06/2021 20.15.37 +0800
+ * @FilePath: /qiokian/tests/qiokian.test.js
  */
 
 /* eslint-disable no-undef */
 
-'use strict'
-
-require('mocha')
+import qiokian from '../src/qiokian.vue'
 var assert = require('assert')
-var qiokian = require('..')
 
 describe('qiokian', function() {
 	// it('should export a function', function() {
